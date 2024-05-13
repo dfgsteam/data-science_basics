870ed0e5094ed2418acaaea82e56bc5989f5f331
Florian Will <florian.will01@stud.uni-goettingen.de>
2024-05-13 18:29:48 +0200

    3 done

---
+++ week04/cleaner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Eingabedatei
+input_file="titanic.csv"
+
+# Ausgabedatei
+output_file="newtitanic.csv"
+
+# Umwandlung von ; zu ,
+sed 's/;/,/g' "$input_file" > temp.csv
+
+# Korrektur von Zeilenumbrüchen innerhalb der Felder
+awk 'BEGIN { FS = OFS = "," } { if (substr($0, length($0), 1) != "\"") { print } else { printf("%s", $0) } }' temp.csv > "$output_file"
+
+# Aufräumen
+rm temp.csv
+
+echo "Die Konvertierung wurde abgeschlossen. Die Ausgabedatei lautet: $output_file"
